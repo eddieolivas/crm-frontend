6aca2f5f7bf3cdfe377f63966502370c3150b223
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-09-14 23:43:19 -0700

    Removed unused Entry component.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import './App.css';
 import { DefaultLayout } from './layouts/DefaultLayout';
 import { Dashboard } from './pages/dashboard/Dashboard';
 
-import { Entry } from './pages/entry/Entry';
-
 function App() {
   return (
     <div className="App">
       <DefaultLayout>
-        {/* <Entry /> */}
         <Dashboard />
       </DefaultLayout>
     </div>
