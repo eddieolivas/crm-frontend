456afe430d817954e688ed2009dd0bdfc5b15f6d
Eddie Olivas <eddie@chrysaliswebdevelopment.com>
2021-11-26 15:03:17 -0800

    Updated API URL to run on AWS EC2.

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const rootUrl = "http://localhost:3001/v1/";
+const rootUrl = "http://crm-client-side-api:3001/v1/";
 const loginUrl = `${rootUrl}user/login`;
 const userProfileUrl = `${rootUrl}user`;
 const logoutUrl = `${rootUrl}user/logout`;
